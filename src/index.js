efc480e1acf45381bd8776b056d653b4f997c9e8
skowolsk <31890900+skowolsk@users.noreply.github.com>
2017-09-29 18:29:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let temp = str.split('');
+    let check = bracketsConfig;
+    let array = [];
+
+
+    for(let i = 0; i < temp.length; i++){
+        for(let j = 0; j < check.length; j++){
+            if (!(temp[i] === check[j][1] && array[array.length - 1] === check[j][0])) {
+                if (temp[i] === check[j][0])
+                    array.push(str[i]);
+            } else array.pop();
+        }
+    }
+
+    if (temp.length % 2 !== 0){
+        return false;
+    }
+    if(array.length === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+};
